37ad742485c0e69bd64c45830d42e7889bc45b74
JessBot500 <jessica.bergstrom20@outlook.com>
2020-06-20 16:21:32 -0500

    current date added to top of page

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+//$(document).ready(statement);
+
 // Display current day at the top of calendar
-var rightNow = new Date();
-console.log(rightNow);
-console.log(typeof rightNow);
+var rightNow = moment().format('dddd, MMMM Do YYYY');
+$("#currentDay").text(rightNow)
+
 
 // Add time blocks for standard business hours
+
 
 
 // Color-code time blocks; past, present, future
 
 
+
 // Enter events in time block
+// $(".list-group").on("click", "p", function () {
+//     console.log("<p> was clicked");
+// });
 
 
 // Save event information in local storage
+
 
 
 // Events saved upon refreshing page
