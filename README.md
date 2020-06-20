3ca05e46e56b29f17f57feee72de491e3e29a60d
JessBot500 <jessica.bergstrom20@outlook.com>
2020-06-19 20:20:10 -0500

    adding current day

---
+++ README.md
@@ -0,0 +1,8 @@
+# Work Day Scheduler Starter Code
+
+* Display current day at the top of calendar
+* Add time blocks for standard business hours
+* Color-code time blocks; past, present, future
+* Enter events in time block
+* Save event information in local storage
+* Events saved upon refreshing page
---
+++ script.js
@@ -0,0 +1,18 @@
+// Display current day at the top of calendar
+var rightNow = new Date();
+console.log(rightNow);
+console.log(typeof rightNow);
+
+// Add time blocks for standard business hours
+
+
+// Color-code time blocks; past, present, future
+
+
+// Enter events in time block
+
+
+// Save event information in local storage
+
+
+// Events saved upon refreshing page
